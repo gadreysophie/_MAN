cce060e567932697f10806d85f4f9b8dfb97bb64
Sophie Gadrey <gadreysophie@gmail.com>
2021-09-07 16:21:21 +0200

    Initial commit

---
+++ src/test/java/org/example/FirstPdfTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import com.sun.source.tree.AssertTree;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.swing.text.Document;
+
+public class FirstPdfTest {
+
+    @Test
+    public void addMetaData (){
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
